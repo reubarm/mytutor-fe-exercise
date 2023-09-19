7023d1e46ad3aa829228fc2ab69f01981739cd41
Reuben Armstrong <reuben@reubarm.com>
2023-09-19 17:37:25 +0100

    Refactor & Improve 😇

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -22,11 +22,9 @@ export const Modal: React.FC<ModalProps> = ({
       ></div>
       <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
         <div className="flex flex-col w-full max-w-[500px] mx-4 md:mx-0 bg-white rounded-lg shadow-lg p-6">
-          <p className="my-10 text-xl leading-relaxed text-center text-2xl">
-            {answerFeedback(answers)}
-          </p>
+       
 
-          <code className="inline-flex text-left bg-gray-800 rounded-lg pt-4 px-6 mb-10">
+          <code className="inline-flex text-left bg-gray-800 rounded-lg pt-4 px-6">
             <span className="flex gap-4">
               <span className="shrink-0 text-gray-500">💡</span>
               <div className="overflow-x-auto text-sm text-white">
@@ -42,6 +40,10 @@ export const Modal: React.FC<ModalProps> = ({
               </div>
             </span>
           </code>
+
+          <p className="my-7 text-xl leading-relaxed text-center text-2xl">
+            {answerFeedback(answers)}
+          </p>
 
           <button
             className="px-10 py-2 bg-teal-400 hover:bg-teal-500 font-bold text-white text-lg rounded"
